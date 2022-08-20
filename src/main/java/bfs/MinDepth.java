ad5173f5e90af069be678e7bef8585109c2ca73f
zhangyue1123 <zhangyue1123@shizhuang-inc.com>
2022-08-20 23:17:32 +0800

    BFS求二叉树的最小深度

---
+++ src/main/java/bfs/MinDepth.java
@@ -0,0 +1,61 @@
+package bfs;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class MinDepth {
+
+    public static class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+    public int minDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        int depth = 1;
+        while (!queue.isEmpty()) {
+            int sz = queue.size();
+            for (int i = 0; i < sz; i++) {
+                TreeNode treeNode = queue.poll();
+                if (treeNode.left == null && treeNode.right == null) {
+                    return depth;
+                }
+                if (treeNode.left != null) {
+                    queue.offer(treeNode.left);
+                }
+                if (treeNode.right != null) {
+                    queue.offer(treeNode.right);
+                }
+            }
+            depth++;
+        }
+        return depth;
+    }
+
+    public static void main(String[] args) {
+        //root = [3,9,20,null,null,15,7]
+        TreeNode root = new TreeNode(3);
+        TreeNode left = new TreeNode(9);
+        TreeNode right = new TreeNode(20);
+        root.left = left;
+        root.right = right;
+        TreeNode rightLeft = new TreeNode(15);
+        TreeNode rightRight = new TreeNode(7);
+        right.left = rightLeft;
+        right.right = rightRight;
+        MinDepth minDepth = new MinDepth();
+        System.out.println(minDepth.minDepth(root));
+
+    }
+}
