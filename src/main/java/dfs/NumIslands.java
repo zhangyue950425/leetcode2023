e04fce396e48a0710aa55e2bbb3338b223cb591f
zhangyue1123 <zhangyue1123@shizhuang-inc.com>
2022-08-20 22:57:44 +0800

    DFS求岛屿数量

---
+++ src/main/java/dfs/NumIslands.java
@@ -0,0 +1,47 @@
+package dfs;
+
+public class NumIslands {
+
+    public int numIslands(char[][] grid) {
+        int res = 0;
+        int m = grid.length, n = grid[0].length;
+        // 遍历
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == '1') {
+                    res++;
+                    dfs(grid, i, j);
+                }
+            }
+        }
+        return res;
+    }
+    private void dfs(char[][] grid, int i, int j) {
+        int m = grid.length, n = grid[0].length;
+        if (i < 0 || j < 0 || i >= m || j >= n) {
+            return;
+        }
+        if (grid[i][j] == '0') {
+            return;
+        }
+        grid[i][j] = '0';
+        dfs(grid, i + 1, j);
+        dfs(grid, i, j + 1);
+        dfs(grid, i - 1, j);
+        dfs(grid, i, j - 1);
+    }
+
+    public static void main(String[] args) {
+        /*grid = [
+  ["1","1","1","1","0"],
+  ["1","1","0","1","0"],
+  ["1","1","0","0","0"],
+  ["0","0","0","0","0"]
+]*/
+        NumIslands numIslands = new NumIslands();
+        char[][] grid = new char[][]{{'1','1','1','1','0'},{'1','1','0','1','0'},{'1','1','0','0','0'},{'0','0','0','0','0'}};
+        System.out.println(numIslands.numIslands(grid));
+
+
+    }
+}
