740cca0065958736cc8fc2c2d5fe8ed7485317a3
zhangyue1123 <zhangyue1123@shizhuang-inc.com>
2022-08-21 16:21:32 +0800

    dp求斐波那契函数

---
+++ src/main/java/dp/Fib.java
@@ -0,0 +1,24 @@
+package dp;
+
+public class Fib {
+    public int fib(int n) {
+        if (n == 0 || n == 1) {
+            return n;
+        }
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+
+    public static void main(String[] args) {
+        int n = 2;
+        Fib fib = new Fib();
+        System.out.println(fib.fib(n));
+    }
+
+
+}
