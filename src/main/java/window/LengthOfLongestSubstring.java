14f58f8f8cbef537e3a68ba52caabd201fb52228
zhangyue1123 <zhangyue1123@shizhuang-inc.com>
2022-09-25 17:05:02 +0800

    无重复字符的最长子串

---
+++ src/main/java/window/LengthOfLongestSubstring.java
@@ -0,0 +1,38 @@
+package window;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class LengthOfLongestSubstring {
+    public int lengthOfLongestSubstring(String s) {
+        // 哈希集合，记录每个字符是否出现过
+        Set<Character> set = new HashSet<>();
+        int length = s.length();
+        // 右指针，初始值为 -1，相当于我们在字符串的左边界的左侧，还没有开始移动
+        int rk = -1, ans = 0;
+        for (int i = 0; i < length; i++) {
+            // 左指针向右移动一格，移除一个字符
+            if (i != 0) {
+                set.remove(s.charAt(i - 1));
+            }
+            while (rk + 1 < length && !set.contains(s.charAt(rk + 1))) {
+                // 不断地移动右指针
+                set.add(s.charAt(rk + 1));
+                rk++;
+            }
+            // 第 i 到 rk 个字符是一个极长的无重复字符子串
+            ans = Math.max(ans, rk - i  + 1);
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        String s = "abcabcbb";
+        LengthOfLongestSubstring lengthOfLongestSubstring = new LengthOfLongestSubstring();
+        System.out.println(lengthOfLongestSubstring.lengthOfLongestSubstring(s));
+        s = "bbbbb";
+        System.out.println(lengthOfLongestSubstring.lengthOfLongestSubstring(s));
+        s = "pwwkew";
+        System.out.println(lengthOfLongestSubstring.lengthOfLongestSubstring(s));
+    }
+}
