11ca10ced48e6fc498a4931d07808189487a4786
zhangyue1123 <zhangyue1123@shizhuang-inc.com>
2023-05-21 10:55:23 +0800

    142. 环形链表 II

--- src/main/java/Hot100/DetectCycle.java
+++ src/main/java/Hot100/DetectCycle.java
@@ -1,5 +1,8 @@
 package Hot100;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class DetectCycle {
     static class ListNode {
           int val;
@@ -8,13 +11,38 @@ public class DetectCycle {
               val = x;
               next = null;
          }
+
       }
+
+    /**
+     * 集合，存放链表节点
+     * @param head
+     * @return
+     */
     public static ListNode detectCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return null;
+        }
+        Set<ListNode> set = new HashSet<>();
+        while (head != null) {
+            if (set.contains(head)) {
+                return head;
+            }
+            set.add(head);
+            head = head.next;
+        }
         return null;
     }
 
     public static void main(String[] args) {
-        ListNode root = new ListNode(0);
+        ListNode root = new ListNode(3);
+        ListNode node = new ListNode(2);
+        ListNode node1 = new ListNode(0);
+        ListNode node2 = new ListNode(-4);
+        root.next = node;
+        node.next = node1;
+        node1.next = node2;
+        node2.next = node;
         DetectCycle detectCycle = new DetectCycle();
         System.out.println(detectCycle.detectCycle(root));
     }
