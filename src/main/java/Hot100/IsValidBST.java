518eeaeb38927f5cdf417e14b6c9ceba0e6a248b
zhangyue1123 <zhangyue1123@shizhuang-inc.com>
2023-05-25 17:53:47 +0800

    98. 验证二叉搜索树

--- src/main/java/Hot100/IsValidBST.java
+++ src/main/java/Hot100/IsValidBST.java
@@ -79,21 +79,29 @@ public class IsValidBST {
         root.left = new TreeNode(1);
         root.right = new TreeNode(3);
         //System.out.println(IsValidBST.isValidBST(root));
-        System.out.println(IsValidBST.isValidBST2(root));
+        //System.out.println(IsValidBST.isValidBST2(root));
         root = new TreeNode(5);
         root.left = new TreeNode(1);
         root.right = new TreeNode(4);
         root.right.left = new TreeNode(3);
         root.right.right = new TreeNode(6);
         //System.out.println(IsValidBST.isValidBST(root));
-        System.out.println(IsValidBST.isValidBST2(root));
+        //System.out.println(IsValidBST.isValidBST2(root));
         root = new TreeNode(5);
         root.left = new TreeNode(4);
         root.right = new TreeNode(6);
         root.right.left = new TreeNode(3);
         root.right.right = new TreeNode(7);
-        System.out.println(IsValidBST.isValidBST2(root));
+        //System.out.println(IsValidBST.isValidBST2(root));
         root = new TreeNode(-2147483648);
+        //System.out.println(IsValidBST.isValidBST2(root));
+        root = new TreeNode(5);
+        root.left = new TreeNode(2);
+        root.left.left = new TreeNode(1);
+        root.left.right = new TreeNode(3);
+        root.right = new TreeNode(7);
+        root.right.left = new TreeNode(6);
+        root.right.right = new TreeNode(8);
         System.out.println(IsValidBST.isValidBST2(root));
     }
 
