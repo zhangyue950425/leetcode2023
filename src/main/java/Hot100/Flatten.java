a39d4b50f480ecf6cda9fe4c84c4128f64c7476b
zhangyue1123 <zhangyue1123@shizhuang-inc.com>
2023-05-27 10:48:12 +0800

    114. 二叉树展开为链表,迭代

--- src/main/java/Hot100/Flatten.java
+++ src/main/java/Hot100/Flatten.java
@@ -43,6 +43,31 @@ public class Flatten {
         preorder(root.right);
     }
 
+    /**
+     * 迭代实现先序遍历
+     * @param root
+     */
+    public static void flatten2(TreeNode root) {
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode node = root;
+        while (!stack.isEmpty() || node != null) {
+            while (node != null) {
+                linkedList.addLast(node);
+                stack.push(node);
+                node = node.left;
+            }
+            node = stack.pop();
+            node = node.right;
+
+        }
+        for (int i = 1; i < linkedList.size(); i++) {
+            TreeNode pre = linkedList.get(i - 1);
+            TreeNode cur = linkedList.get(i);
+            pre.left = null;
+            pre.right = cur;
+        }
+    }
+
     public static void main(String[] args) {
         TreeNode root = new TreeNode(1);
         root.left = new TreeNode(2);
@@ -55,12 +80,12 @@ public class Flatten {
         //flatten(root);
         root = new TreeNode(1);
         root.left = new TreeNode(2);
-        root.right = new TreeNode(5);
         root.left.left = new TreeNode(3);
         root.left.right = new TreeNode(4);
         root.right = new TreeNode(5);
         root.right.right = new TreeNode(6);
-        flatten(root);
+        //flatten(root);
+        flatten2(root);
     }
 
 }
