65c2fb955bbc772bbe33d2ef138c923a057e69e8
zhangyue1123 <zhangyue1123@shizhuang-inc.com>
2023-05-28 15:42:50 +0800

    搜索二维矩阵 II:z字型查找

--- src/main/java/Hot100/SearchMatrix.java
+++ src/main/java/Hot100/SearchMatrix.java
@@ -49,15 +49,39 @@ public class SearchMatrix {
         return false;
     }
 
+    /**
+     * z字型查找
+     * @param matrix
+     * @param target
+     * @return
+     */
+    public boolean searchMatrix3(int[][] matrix, int target) {
+        int row = matrix.length;
+        int col = matrix[0].length;
+        int i = 0, j = col - 1;
+        while (i < row && j >= 0) {
+            if (target == matrix[i][j]) {
+                return true;
+            } else if (target > matrix[i][j]) {
+                i++;
+            } else {
+                j--;
+            }
+        }
+        return false;
+    }
+
     public static void main(String[] args) {
         int[][] matrix = new int[][]{{1,4,7,11,15}, {2,5,8,12,19}, {3,6,9,16,22}, {10,13,14,17,24}, {18,21,23,26,30}};
         int target = 5;
         SearchMatrix searchMatrix = new SearchMatrix();
         System.out.println(searchMatrix.searchMatrix(matrix, target));
         System.out.println(searchMatrix.searchMatrix2(matrix, target));
+        System.out.println(searchMatrix.searchMatrix3(matrix, target));
         target = 20;
         System.out.println(searchMatrix.searchMatrix(matrix, target));
         System.out.println(searchMatrix.searchMatrix2(matrix, target));
+        System.out.println(searchMatrix.searchMatrix3(matrix, target));
 
     }
 }
