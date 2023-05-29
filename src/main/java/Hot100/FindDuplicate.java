f95f788fd831aa1f85e6e1b203cdfaacb90937a1
zhangyue1123 <zhangyue1123@shizhuang-inc.com>
2023-05-29 16:12:47 +0800

    287. 寻找重复数，二分查找

--- src/main/java/Hot100/FindDuplicate.java
+++ src/main/java/Hot100/FindDuplicate.java
@@ -18,11 +18,39 @@ public class FindDuplicate {
         return nums[length - 1];
     }
 
+    /**
+     * 二分查找实现
+     * @param nums
+     * @return
+     */
+    public int findDuplicate2(int[] nums) {
+        int length = nums.length;;
+        int left = 1;
+        int right = length - 1;
+        while (left < right) {
+            int mid = (left  + right) / 2;
+            int count = 0;
+            for (int i = 0; i < length; i++) {
+                if (nums[i] <= mid) {
+                    count++;
+                }
+            }
+            if (count > mid) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+
     public static void main(String[] args) {
         int[] nums = new int[] {1,3,4,2,2};
         FindDuplicate findDuplicate = new FindDuplicate();
         System.out.println(findDuplicate.findDuplicate(nums));
+        System.out.println(findDuplicate.findDuplicate2(nums));
         nums = new int[]{3,1,3,4,2};
         System.out.println(findDuplicate.findDuplicate(nums));
+        System.out.println(findDuplicate.findDuplicate2(nums));
     }
 }
