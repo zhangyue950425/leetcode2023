9f21afece56acf7174925f8174488d7068b6e140
zhangyue1123 <zhangyue1123@shizhuang-inc.com>
2023-06-02 21:00:19 +0800

    initiate

---
+++ src/main/java/Hot100/CalcEquation.java
@@ -0,0 +1,20 @@
+package Hot100;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class CalcEquation {
+
+    public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
+
+    }
+
+    public static void main(String[] args) {
+        List<List<String>> equations = new ArrayList<>();
+        double[] values = new double[] {};
+        List<List<String>> queries = new ArrayList<>();
+        CalcEquation calcEquation = new CalcEquation();
+        System.out.println(Arrays.toString(calcEquation.calcEquation(equations, values, queries)));
+    }
+}
