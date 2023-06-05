4f248bee428cb9c116991a6991505d6f76484772
zhangyue1123 <zhangyue1123@shizhuang-inc.com>
2023-06-05 13:07:52 +0800

    662. 二叉树最大宽度,BFS

--- src/main/java/TenDaysByteDancePractice/WidthOfBinaryTree.java
+++ src/main/java/TenDaysByteDancePractice/WidthOfBinaryTree.java
@@ -1,5 +1,10 @@
 package TenDaysByteDancePractice;
 
+import javafx.util.Pair;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class WidthOfBinaryTree {
 
     public static class TreeNode {
@@ -16,16 +21,41 @@ public class WidthOfBinaryTree {
   }
 
     /**
-     * BFS
+     * BFS，节点编号
      * @param root
      * @return
      */
     public static int widthOfBinaryTree(TreeNode root) {
-
+        int max = 1;
+        List<Pair<TreeNode, Integer>> list = new ArrayList<>();
+        // 给根节点编号为1，左子节点位2 * 1，右子节点编号为2 * 1 + 1
+        list.add(new Pair<>(root, 1));
+        while (!list.isEmpty()) {
+            List<Pair<TreeNode, Integer>> list1 = new ArrayList<>();
+            for (Pair<TreeNode, Integer> pair : list) {
+                TreeNode node = pair.getKey();
+                Integer index = pair.getValue();
+                if (node.left != null) {
+                    list1.add(new Pair<>(node.left, index * 2));
+                }
+                if (node.right != null) {
+                    list1.add(new Pair<>(node.right, index * 2 + 1));
+                }
+            }
+            max = Math.max(max, list.get(list.size() - 1).getValue() - list.get(0).getValue() + 1);
+            list = list1;
+        }
+        return max;
     }
 
     public static void main(String[] args) {
-        TreeNode root = new TreeNode();
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(3);
+        root.right = new TreeNode(2);
+        root.left.left = new TreeNode(5);
+        root.left.left.left = new TreeNode(6);
+        root.right.right = new TreeNode(9);
+        root.right.right.left = new TreeNode(7);
         System.out.println(WidthOfBinaryTree.widthOfBinaryTree(root));
     }
 }
