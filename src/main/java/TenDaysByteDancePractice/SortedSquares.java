5dff61d1449bccf59d8b05fc902a10b702b19569
zhangyue1123 <zhangyue1123@shizhuang-inc.com>
2023-06-08 16:06:54 +0800

    977. 有序数组的平方,双指针

--- src/main/java/TenDaysByteDancePractice/SortedSquares.java
+++ src/main/java/TenDaysByteDancePractice/SortedSquares.java
@@ -27,6 +27,72 @@ public class SortedSquares {
         return result;
     }
 
+    /**
+     * 双指针
+     * @param nums
+     * @return
+     */
+    public int[] sortedSquares2(int[] nums) {
+        int length = nums.length;
+        int[] result = new int[length];
+        if (length == 1) {
+            result[0] = (int) Math.pow(Math.abs(nums[0]), 2);
+            return result;
+        }
+        // index指向最小的正整数或0
+        int index = 0;
+        while (index < length) {
+            if (nums[index] >= 0) {
+                break;
+            } else {
+                index++;
+            }
+        }
+        int left = index - 1;
+        int right = index;
+        index = 0;
+        while (left >= 0 && right <= length - 1 && index < length) {
+            if (Math.abs(nums[left]) >= nums[right]) {
+                result[index++] = (int) Math.pow(nums[right], 2);
+                right++;
+            } else {
+                result[index++] = (int) (Math.pow(Math.abs(nums[left]), 2));
+                left--;
+            }
+        }
+        if (left > 0) {
+            result[length - 1] = (int) (Math.pow(Math.abs(nums[left]), 2));
+        }
+        if (right == length - 1) {
+            result[length - 1] = (int) Math.pow(nums[right], 2);
+        }
+        return result;
+    }
+
+    /**
+     * 双指针
+     * @param nums
+     * @return
+     */
+    public int[] sortedSquares3(int[] nums) {
+       int length = nums.length;
+       int[] result = new int[length];
+       int index = length - 1;
+       for (int i = 0, j = length - 1; i <= j && i < length && j < length;) {
+           int productI = nums[i] * nums[i];
+           int productJ = nums[j] * nums[j];
+           if (productI > productJ) {
+               result[index] = productI;
+               i++;
+           } else {
+               result[index] = productJ;
+               j--;
+           }
+           index--;
+       }
+       return result;
+    }
+
     public static void main(String[] args) {
         // 输入：nums = [-4,-1,0,3,10]
         // 输出：[0,1,9,16,100]
@@ -36,7 +102,15 @@ public class SortedSquares {
         int[] nums = new int[] {-4,-1,0,3,10};
         SortedSquares sortedSquares = new SortedSquares();
         System.out.println(Arrays.toString(sortedSquares.sortedSquares(nums)));
+        //System.out.println(Arrays.toString(sortedSquares.sortedSquares2(nums)));
+        System.out.println(Arrays.toString(sortedSquares.sortedSquares3(nums)));
         nums = new int[] {-7,-3,2,3,11};
         System.out.println(Arrays.toString(sortedSquares.sortedSquares(nums)));
+        //System.out.println(Arrays.toString(sortedSquares.sortedSquares2(nums)));
+        System.out.println(Arrays.toString(sortedSquares.sortedSquares3(nums)));
+        nums = new int[] {-1};
+        System.out.println(Arrays.toString(sortedSquares.sortedSquares(nums)));
+        //System.out.println(Arrays.toString(sortedSquares.sortedSquares2(nums)));
+        System.out.println(Arrays.toString(sortedSquares.sortedSquares3(nums)));
     }
 }
