b1e4cde426902262cf1dd6f2dc6fcc921e718ae6
zhangyue1123 <zhangyue1123@shizhuang-inc.com>
2023-06-08 12:39:21 +0800

    977. 有序数组的平方,快排

---
+++ src/main/java/TenDaysByteDancePractice/SortedSquares.java
@@ -0,0 +1,42 @@
+package TenDaysByteDancePractice;
+
+import java.util.Arrays;
+
+/**
+ * 977. 有序数组的平方:
+ * 给你一个按 非递减顺序 排序的整数数组 nums，
+ * 返回 每个数字的平方 组成的新数组，要求也按 非递减顺序 排序。
+ * 1 <= nums.length <= 104
+ * -104 <= nums[i] <= 104
+ * nums 已按 非递减顺序 排序
+ * 时间复杂度为 O(n) 的算法
+ */
+public class SortedSquares {
+    /**
+     * 快排
+     * @param nums
+     * @return
+     */
+    public int[] sortedSquares(int[] nums) {
+        int length = nums.length;
+        int[] result = new int[length];
+        for (int i = 0; i < length; i++) {
+            result[i] = (int) Math.pow(nums[i], 2);
+        }
+        Arrays.sort(result);
+        return result;
+    }
+
+    public static void main(String[] args) {
+        // 输入：nums = [-4,-1,0,3,10]
+        // 输出：[0,1,9,16,100]
+        // 解释：平方后，数组变为 [16,1,0,9,100]排序后，数组变为 [0,1,9,16,100]
+        // 输入：nums = [-7,-3,2,3,11]
+        // 输出：[4,9,9,49,121]
+        int[] nums = new int[] {-4,-1,0,3,10};
+        SortedSquares sortedSquares = new SortedSquares();
+        System.out.println(Arrays.toString(sortedSquares.sortedSquares(nums)));
+        nums = new int[] {-7,-3,2,3,11};
+        System.out.println(Arrays.toString(sortedSquares.sortedSquares(nums)));
+    }
+}
