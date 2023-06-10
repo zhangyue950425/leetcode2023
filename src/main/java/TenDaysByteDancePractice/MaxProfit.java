f7bdfbcdd453df6c55052e5d53865a56f2a52bb4
zhangyue1123 <zhangyue1123@shizhuang-inc.com>
2023-06-10 09:36:14 +0800

    121. 买卖股票的最佳时机:动态规划二维数组

--- src/main/java/TenDaysByteDancePractice/MaxProfit.java
+++ src/main/java/TenDaysByteDancePractice/MaxProfit.java
@@ -54,6 +54,29 @@ public class MaxProfit {
        return result;
     }
 
+    /**
+     * 动态规划:二维数组
+     * @param prices
+     * @return
+     */
+    public int maxProfit3(int[] prices) {
+        int length = prices.length;
+        // dp[i][0]:表示今天最大利润，是在今天不持有股票的情况下，则昨天的情况有两种：
+        // 一是昨天获得最大利润的时候是在持有股票但是今天卖出，二是昨天也不持有股票所以今天啥都没做
+        // dp[i][1]:表示今天最大利润，是在今天持有股票的情况下：
+        // 则昨天的情况有两种，一是昨天获得最大利润的时候是在没有股票，今天买入股票，因为只能交易一次，所以今天买入的话只能从今天算起来利润，
+        // 二是昨天持有股票今天啥都没做
+        int[][] dp = new int[length][2];
+        // 初始化
+        dp[0][0] = 0;
+        dp[0][1] = -prices[0];
+        for (int i = 1; i < length; i++) {
+            dp[i][0] = Math.max(dp[i - 1][1] + prices[i], dp[i - 1][0]);
+            dp[i][1] = Math.max(- prices[i], dp[i - 1][1]);
+        }
+        return Math.max(dp[length - 1][1], dp[length - 1][0]);
+    }
+
     public static void main(String[] args) {
         // 输入：[7,1,5,3,6,4]
         // 输出：5
@@ -65,12 +88,15 @@ public class MaxProfit {
         int[] prices = new int[] {7,1,5,3,6,4};
         MaxProfit maxProfit = new MaxProfit();
         System.out.println(maxProfit.maxProfit2(prices));
+        System.out.println(maxProfit.maxProfit3(prices));
         //System.out.println(maxProfit.maxProfit(prices));
         prices = new int[] {7,6,4,3,1};
         System.out.println(maxProfit.maxProfit2(prices));
+        System.out.println(maxProfit.maxProfit3(prices));
         //System.out.println(maxProfit.maxProfit(prices));
         prices = new int[] {2,4,1};
         System.out.println(maxProfit.maxProfit2(prices));
+        System.out.println(maxProfit.maxProfit3(prices));
         //System.out.println(maxProfit.maxProfit(prices));
     }
 }
