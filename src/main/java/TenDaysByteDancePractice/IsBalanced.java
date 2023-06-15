81e9bebeee2ffd44d9f20ecbad3fe165f31028de
zhangyue1123 <zhangyue1123@shizhuang-inc.com>
2023-06-15 17:40:39 +0800

    剑指 Offer 55 - II. 平衡二叉树：后序遍历

--- src/main/java/TenDaysByteDancePractice/IsBalanced.java
+++ src/main/java/TenDaysByteDancePractice/IsBalanced.java
@@ -28,6 +28,34 @@ public class IsBalanced {
       return Math.abs(left - right) > 1 ? -1 : Math.max(left, right) + 1;
     }
 
+  /**
+   * 从顶置底，计算每个节点的左右子树深度差是否满足小于等于1，不满足则肯定不是平衡二叉树
+   * @param root
+   * @return
+   */
+  public static boolean isBalanced2(TreeNode root) {
+    if (root == null) {
+      return true;
+    }
+    int left = height(root.left);
+    int right = height(root.right);
+    return Math.abs(left - right) <= 1 && isBalanced2(root.left) && isBalanced2(root.right);
+  }
+
+  /**
+   * 深度计算
+   * @param root
+   * @return
+   */
+  private static int height(TreeNode root) {
+    if (root == null) {
+      return 0;
+    }
+    int left = height(root.left);
+    int right = height(root.right);
+    return Math.max(left, right) + 1;
+  }
+
 
   public static void main(String[] args) {
     TreeNode root = new TreeNode(3);
@@ -36,6 +64,7 @@ public class IsBalanced {
     root.right.left = new TreeNode(15);
     root.right.right = new TreeNode(7);
     System.out.println(IsBalanced.isBalanced(root));
+    System.out.println(IsBalanced.isBalanced2(root));
     // 1,2,2,3,3,null,null,4,4
     root = new TreeNode(1);
     root.left = new TreeNode(2);
@@ -45,5 +74,6 @@ public class IsBalanced {
     root.left.left.left = new TreeNode(4);
     root.left.left.right = new TreeNode(4);
     System.out.println(IsBalanced.isBalanced(root));
+    System.out.println(IsBalanced.isBalanced2(root));
   }
 }
