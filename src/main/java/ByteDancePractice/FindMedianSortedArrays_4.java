d55faaa11d2c0f34d638290b50164990ec6ea8e1
zhangyue1123 <zhangyue1123@shizhuang-inc.com>
2023-06-22 12:42:29 +0800

    4. 寻找两个正序数组的中位数:暴力解法

--- src/main/java/ByteDancePractice/FindMedianSortedArrays_4.java
+++ src/main/java/ByteDancePractice/FindMedianSortedArrays_4.java
@@ -49,16 +49,46 @@ public class FindMedianSortedArrays_4 {
         return num;
     }
 
+    /**
+     * 暴力解法：直接找中位数的位置
+     * @param nums1
+     * @param nums2
+     * @return
+     */
+    public double findMedianSortedArrays2(int[] nums1, int[] nums2) {
+        int length1 = nums1.length;
+        int length2 = nums2.length;
+        int length = length1 + length2;
+        int left = -1, right = -1;
+        int start1 = 0, start2 = 0;
+        for (int i = 0; i <= length / 2; i++) {
+            left = right;
+            if (start1 < length1 && (start2 >= length2 || nums1[start1] < nums2[start2])) {
+                right = nums1[start1++];
+            } else {
+                right = nums2[start2++];
+            }
+        }
+        if (length % 2 == 0) {
+            return (left + right) / 2.0;
+        } else {
+            return right;
+        }
+    }
+
     public static void main(String[] args) {
         FindMedianSortedArrays_4 findMedianSortedArrays4 = new FindMedianSortedArrays_4();
         int[] nums1 = new int[] {1,3};
         int[] nums2 = new int[] {2};
         System.out.println(findMedianSortedArrays4.findMedianSortedArrays(nums1, nums2));
+        System.out.println(findMedianSortedArrays4.findMedianSortedArrays2(nums1, nums2));
         nums1 = new int[] {1,2};
         nums2 = new int[] {3,4};
         System.out.println(findMedianSortedArrays4.findMedianSortedArrays(nums1, nums2));
+        System.out.println(findMedianSortedArrays4.findMedianSortedArrays2(nums1, nums2));
         nums1 = new int[] {};
         nums2 = new int[] {2,3};
         System.out.println(findMedianSortedArrays4.findMedianSortedArrays(nums1, nums2));
+        System.out.println(findMedianSortedArrays4.findMedianSortedArrays2(nums1, nums2));
     }
 }
