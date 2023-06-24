e59884bd300c0ddcf574cc1c3483ac9c8978e804
zhangyue1123 <zhangyue1123@shizhuang-inc.com>
2023-06-24 21:31:38 +0800

    25. K 个一组翻转链表:一次遍历

---
+++ src/main/java/ByteDancePractice/ThreeSum_15.java
@@ -0,0 +1,17 @@
+package ByteDancePractice;
+
+import java.util.List;
+
+public class ThreeSum_15 {
+
+    public List<List<Integer>> threeSum(int[] nums) {
+
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[] {};
+        ThreeSum_15 threeSum15 = new ThreeSum_15();
+        System.out.println(threeSum15.threeSum(nums));
+    }
+
+}
