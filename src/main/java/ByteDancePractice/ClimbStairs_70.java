38a23f2095d870495c90a13c3fc83d7f062ce0d2
zhangyue1123 <zhangyue1123@shizhuang-inc.com>
2023-07-01 14:38:30 +0800

    70. 爬楼梯

---
+++ src/main/java/ByteDancePractice/ClimbStairs_70.java
@@ -0,0 +1,33 @@
+package ByteDancePractice;
+
+public class ClimbStairs_70 {
+
+    public int climbStairs(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return 1;
+        }
+        if (n == 2) {
+            return 2;
+        }
+        //dp[i]表示的是爬到第i接台阶有几种方案
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+
+    public static void main(String[] args) {
+        ClimbStairs_70 climbStairs70 = new ClimbStairs_70();
+        int n = 2;
+        System.out.println(climbStairs70.climbStairs(n));
+        n = 3;
+        System.out.println(climbStairs70.climbStairs(n));
+    }
+}
