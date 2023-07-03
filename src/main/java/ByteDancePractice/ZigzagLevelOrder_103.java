06bac9db030eb5ae24fa2c7129f5cdc541434dde
zhangyue1123 <zhangyue1123@shizhuang-inc.com>
2023-07-03 20:58:49 +0800

    103. 二叉树的锯齿形层序遍历:BFS

---
+++ src/main/java/ByteDancePractice/ZigzagLevelOrder_103.java
@@ -0,0 +1,65 @@
+package ByteDancePractice;
+
+import java.util.*;
+
+public class ZigzagLevelOrder_103 {
+
+    public static class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+
+    public static List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+        Deque<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        int depth = 0;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> list = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                if (depth % 2 == 0) {
+                    list.add(node.val);
+                } else {
+                   list.add(0, node.val);
+                }
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+            }
+            depth++;
+            result.add(list);
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(9);
+        root.right = new TreeNode(20);
+        root.right.left = new TreeNode(15);
+        root.right.right = new TreeNode(7);
+        System.out.println(ZigzagLevelOrder_103.zigzagLevelOrder(root));
+        root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.right.right = new TreeNode(5);
+        System.out.println(ZigzagLevelOrder_103.zigzagLevelOrder(root));
+    }
+}
