dbb859a5ec253f853a2151341e18aedd78db7066
zhangyue1123 <zhangyue1123@shizhuang-inc.com>
2023-07-08 10:46:02 +0800

    9. 回文数:整除

--- src/main/java/ByteDancePractice/IsPalindrome_9.java
+++ src/main/java/ByteDancePractice/IsPalindrome_9.java
@@ -25,13 +25,38 @@ public class IsPalindrome_9 {
 
     }
 
+    public boolean isPalindrome2(int x) {
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
+        }
+        int num = 0;
+        while (x > num) {
+            num = num * 10 + x % 10;
+            x = x / 10;
+        }
+        return x == num / 10 || x == num;
+    }
+
+
     public static void main(String[] args) {
         int x = 121;
         IsPalindrome_9 isPalindrome9 = new IsPalindrome_9();
         System.out.println(isPalindrome9.isPalindrome(x));
+        System.out.println(isPalindrome9.isPalindrome2(x));
         x = -121;
         System.out.println(isPalindrome9.isPalindrome(x));
+        System.out.println(isPalindrome9.isPalindrome2(x));
         x = 10;
         System.out.println(isPalindrome9.isPalindrome(x));
+        System.out.println(isPalindrome9.isPalindrome2(x));
+        x = 11;
+        System.out.println(isPalindrome9.isPalindrome(x));
+        System.out.println(isPalindrome9.isPalindrome2(x));
+        x = 111;
+        System.out.println(isPalindrome9.isPalindrome(x));
+        System.out.println(isPalindrome9.isPalindrome2(x));
+        x = 1111;
+        System.out.println(isPalindrome9.isPalindrome(x));
+        System.out.println(isPalindrome9.isPalindrome2(x));
     }
 }
