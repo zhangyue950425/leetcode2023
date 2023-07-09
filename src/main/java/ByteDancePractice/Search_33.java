c5e08525bac6a0917e1ac134ce70247c7d07dca5
zhangyue1123 <zhangyue1123@shizhuang-inc.com>
2023-07-09 17:00:07 +0800

    33. 搜索旋转排序数组:二分法

--- src/main/java/ByteDancePractice/Search_33.java
+++ src/main/java/ByteDancePractice/Search_33.java
@@ -39,7 +39,48 @@ public class Search_33 {
         return -1;
     }
 
+    /**
+     * 二分法
+     * @param nums
+     * @param target
+     * @return
+     */
     public int search2(int[] nums, int target) {
+        int length = nums.length;
+        if (length == 0) {
+            return -1;
+        }
+        if (length == 1) {
+            return nums[0] == target ? 0 : -1;
+        }
+        // 找到旋转点
+        int index = length - 1;
+        for (int i = 1; i < length; i++) {
+            if (nums[i] < nums[i - 1]) {
+                index = i;
+                break;
+            }
+        }
+        int left = 0;
+        int right = length - 1;
+        if (target >= nums[0]) {
+            left = 0;
+            right = index;
+        } else {
+            left = index;
+            right = length - 1;
+        }
+        while (left <= right) {
+            int mid = (right + left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target){
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return -1;
 
     }
 
@@ -48,11 +89,22 @@ public class Search_33 {
         int[] nums = new int[] {4,5,6,7,0,1,2};
         int target = 0;
         System.out.println(search33.search(nums, target));
+        System.out.println(search33.search2(nums, target));
         nums = new int[] {4,5,6,7,0,1,2};
         target = 3;
         System.out.println(search33.search(nums, target));
+        System.out.println(search33.search2(nums, target));
         nums = new int[] {1};
         target = 0;
         System.out.println(search33.search(nums, target));
+        System.out.println(search33.search2(nums, target));
+        nums = new int[]{1,3};
+        target = 3;
+        System.out.println(search33.search(nums, target));
+        System.out.println(search33.search2(nums, target));
+        nums = new int[]{1,3};
+        target = 0;
+        System.out.println(search33.search(nums, target));
+        System.out.println(search33.search2(nums, target));
     }
 }
