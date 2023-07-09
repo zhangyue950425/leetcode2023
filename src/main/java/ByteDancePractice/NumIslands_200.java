a7763a188355be8b87e313a830cbf1f853a864c4
zhangyue1123 <zhangyue1123@shizhuang-inc.com>
2023-07-09 14:09:20 +0800

    200. 岛屿数量:dfs

---
+++ src/main/java/ByteDancePractice/NumIslands_200.java
@@ -0,0 +1,51 @@
+package ByteDancePractice;
+
+public class NumIslands_200 {
+
+    public int numIslands(char[][] grid) {
+        int row = grid.length;
+        int col = grid[0].length;
+        int count = 0;
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (grid[i][j] == '1') {
+                    dfs(grid, i, j);
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    /**
+     * dfs遍历网格，(r,c)为当前位置，需要向该位置的上下左右遍历，对于遍历过的格子设置标志
+     * 递归结束在，遍历的格子超过范围，或者是已经遍历过该位置
+     * @param grid
+     * @param r
+     * @param c
+     */
+    private void dfs(char[][] grid, int r, int c) {
+        if (r < 0 || c < 0 || r >= grid.length || c >= grid[0].length) {
+            return;
+        }
+        if (grid[r][c] != '1') {
+            return;
+        }
+        grid[r][c] = '2';
+        dfs(grid, r - 1, c);
+        dfs(grid, r + 1, c);
+        dfs(grid, r, c - 1);
+        dfs(grid, r, c + 1);
+    }
+
+    public static void main(String[] args) {
+        NumIslands_200 numIslands200 = new NumIslands_200();
+        char[][] grid = new char[][] {
+                {'1','1','0','0','0'},
+                {'1','1','0','0','0'},
+                {'0','0','1','0','0'},
+                {'0','0','0','1','1'}
+        };
+        System.out.println(numIslands200.numIslands(grid));
+    }
+}
