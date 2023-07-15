d8dc956f62d581f56b4f5ecadbea621f7d7e9275
zhangyue1123 <zhangyue1123@shizhuang-inc.com>
2023-07-15 09:03:38 +0800

    135. 分发糖果:贪心算法

--- src/main/java/ByteDancePractice/Candy_135.java
+++ src/main/java/ByteDancePractice/Candy_135.java
@@ -2,12 +2,41 @@ package ByteDancePractice;
 
 public class Candy_135 {
     public int candy(int[] ratings) {
-        return 0;
+        int n = ratings.length;
+        int[] candys = new int[n];
+        for (int i = 0; i < n; i++) {
+            candys[i] = 1;
+        }
+        for (int i = 1; i < n; i++) {
+            if (ratings[i] > ratings[i - 1]) {
+                candys[i] = candys[i - 1] + 1;
+            }
+        }
+        int[] candys2 = new int[n];
+        for (int i = 0; i < n; i++) {
+            candys2[i] = 1;
+        }
+        for (int i = n - 2; i >= 0; i--) {
+            if (ratings[i] > ratings[i + 1]) {
+                candys2[i] = candys2[i + 1] + 1;
+            }
+        }
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum = sum + Math.max(candys[i], candys2[i]);
+        }
+        return sum;
     }
 
     public static void main(String[] args) {
         Candy_135 candy135 = new Candy_135();
-        int[] ratings = new int[] {};
+        int[] ratings = new int[] {1, 0, 2};
+        System.out.println(candy135.candy(ratings));
+        ratings = new int[] {1,2,2};
+        System.out.println(candy135.candy(ratings));
+        ratings = new int[] {1,3,2,2,1};
+        System.out.println(candy135.candy(ratings));
+        ratings = new int[] {1,2,87,87,87,2,1};
         System.out.println(candy135.candy(ratings));
     }
 }
