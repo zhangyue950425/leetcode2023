c4c1014de3f67ccba84c2210b8504c2dfe5eb2d3
zhangyue1123 <zhangyue1123@shizhuang-inc.com>
2023-07-31 17:23:56 +0800

    33. 搜索旋转排序数组：二分

--- src/main/java/ByteDancePractice/Search_33_2.java
+++ src/main/java/ByteDancePractice/Search_33_2.java
@@ -50,22 +50,58 @@ public class Search_33_2 {
         return -1;
     }
 
+    public int search2(int[] nums, int target) {
+        int length = nums.length;
+        if (nums == null || length == 0) {
+            return -1;
+        }
+        int start = 0;
+        int end = length - 1;
+        int mid;
+        while (start <= end) {
+            mid = (start + end) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            // 前半部分有序
+            if (nums[start] <= nums[mid]) {
+                if (target >= nums[start] && target < nums[mid]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            } else {
+                if (target > nums[mid] && target <= nums[end]) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+
     public static void main(String[] args) {
         Search_33_2 search332 = new Search_33_2();
         int[] nums = new int[] {4,5,6,7,0,1,2};
         int target = 0;
-        System.out.println(search332.search(nums, target));
+        //System.out.println(search332.search(nums, target));
+        System.out.println(search332.search2(nums, target));
         nums = new int[] {4,5,6,7,0,1,2};
         target = 3;
-        System.out.println(search332.search(nums, target));
+        //System.out.println(search332.search(nums, target));
+        System.out.println(search332.search2(nums, target));
         nums = new int[] {1};
         target = 0;
-        System.out.println(search332.search(nums, target));
+        //System.out.println(search332.search(nums, target));
+        System.out.println(search332.search2(nums, target));
         nums = new int[] {1,3};
         target = 3;
-        System.out.println(search332.search(nums, target));
+        //System.out.println(search332.search(nums, target));
+        System.out.println(search332.search2(nums, target));
         nums = new int[] {3, 5, 1};
         target = 5;
-        System.out.println(search332.search(nums, target));
+        //System.out.println(search332.search(nums, target));
+        System.out.println(search332.search2(nums, target));
     }
 }
