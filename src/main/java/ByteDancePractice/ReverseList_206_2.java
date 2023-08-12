8235b21329f6b8ca5f686e86b44a03c84b851006
zhangyue1123 <zhangyue1123@shizhuang-inc.com>
2023-08-12 12:02:44 +0800

    92. 反转链表:递归

--- src/main/java/ByteDancePractice/ReverseList_206_2.java
+++ src/main/java/ByteDancePractice/ReverseList_206_2.java
@@ -30,6 +30,24 @@ public class ReverseList_206_2 {
         return pre;
     }
 
+    /**
+     * 递归
+     * @param head
+     * @return
+     */
+    public ListNode reverseList2(ListNode head) {
+        return recur(head, null);
+    }
+
+    private ListNode recur(ListNode cur, ListNode pre) {
+        if (cur == null) {
+            return pre;
+        }
+        ListNode res = recur(cur.next, cur);
+        cur.next = pre;
+        return res;
+    }
+
     public static void main(String[] args) {
         ReverseList_206_2 reverseList_206_2 = new ReverseList_206_2();
         ListNode head = new ListNode(1);
@@ -38,6 +56,13 @@ public class ReverseList_206_2 {
         head.next.next.next = new ListNode(4);
         head.next.next.next.next = new ListNode(5);
         ListNode reverseNode = reverseList_206_2.reverseList(head);
+        System.out.println(reverseNode);
+        head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+        reverseNode = reverseList_206_2.reverseList2(head);
         System.out.println(reverseNode);
     }
 
