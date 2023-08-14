27d4384bd110234b75c352c422dbb258e89d1f4f
zhangyue1123 <zhangyue1123@shizhuang-inc.com>
2023-08-14 16:56:41 +0800

    33

---
+++ src/main/java/ByteDancePractice/Search_33_3.java
@@ -0,0 +1,15 @@
+package ByteDancePractice;
+
+public class Search_33_3 {
+
+    public int search(int[] nums, int target) {
+
+    }
+
+    public static void main(String[] args) {
+        Search_33_3 search_33_3 = new Search_33_3();
+        int[] nums = new int[] {};
+        int target = ;
+        System.out.println(search_33_3.search(nums, target));
+    }
+}
