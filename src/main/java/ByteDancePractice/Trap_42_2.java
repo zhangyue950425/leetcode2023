52123ddb31dc2fd616d8b972ca0357ac8836fdc9
zhangyue1123 <zhangyue1123@shizhuang-inc.com>
2023-08-16 18:18:08 +0800

    42. 接雨水

---
+++ src/main/java/ByteDancePractice/Trap_42_2.java
@@ -0,0 +1,16 @@
+package ByteDancePractice;
+
+public class Trap_42_2 {
+
+    public int trap(int[] height) {
+
+    }
+
+    public static void main(String[] args) {
+        Trap_42_2 trap_42_2 = new Trap_42_2();
+        int[] height = new int[] {0,1,0,2,1,0,1,3,2,1,2,1};
+        System.out.println(trap_42_2.trap(height));
+        height = new int[] {4,2,0,3,2,5};
+        System.out.println(trap_42_2.trap(height));
+    }
+}
