083fed9cc9a7193a68a6df136662b4a70bf50eb9
zhangyue1123 <zhangyue1123@shizhuang-inc.com>
2023-08-18 11:17:38 +0800

    42. 接雨水:单调栈

--- src/main/java/ByteDancePractice/Trap_42_2.java
+++ src/main/java/ByteDancePractice/Trap_42_2.java
@@ -1,9 +1,34 @@
 package ByteDancePractice;
 
+import java.util.Stack;
+
 public class Trap_42_2 {
 
+    /**
+     * 栈
+     * @param height
+     * @return
+     */
     public int trap(int[] height) {
-
+        int length = height.length;
+        Stack<Integer> stack = new Stack<>();
+        int cur = 0;
+        int sum = 0;
+        while (cur < length) {
+            while (!stack.isEmpty() && height[cur] > height[stack.peek()]) {
+                int h = height[stack.peek()];
+                stack.pop();
+                if (stack.isEmpty()) {
+                    break;
+                }
+                int distance = cur - stack.peek() - 1;
+                int min = Math.min(height[cur], height[stack.peek()]);
+                sum = sum + distance * (min - h);
+            }
+            stack.push(cur);
+            cur++;
+        }
+        return sum;
     }
 
     public static void main(String[] args) {
