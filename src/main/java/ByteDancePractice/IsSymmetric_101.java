909374d124ca711f441c97b34769add291985164
zhangyue1123 <zhangyue1123@shizhuang-inc.com>
2023-08-28 12:10:49 +0800

    101. 对称二叉树：BFS

--- src/main/java/ByteDancePractice/IsSymmetric_101.java
+++ src/main/java/ByteDancePractice/IsSymmetric_101.java
@@ -1,5 +1,8 @@
 package ByteDancePractice;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class IsSymmetric_101 {
 
     public static class TreeNode {
@@ -40,6 +43,38 @@ public class IsSymmetric_101 {
         return dfs(left.left, right.right) && dfs(left.right, right.left);
     }
 
+    /**
+     * DFS
+     * @param root
+     * @return
+     */
+    public boolean isSymmetric2(TreeNode root) {
+        if (root == null || (root.left == null && root.right == null)) {
+            return true;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root.left);
+        queue.offer(root.right);
+        while (!queue.isEmpty()) {
+            TreeNode left = queue.poll();
+            TreeNode right = queue.poll();
+            if (left == null && right == null) {
+                continue;
+            }
+            if (left == null || right == null) {
+                return false;
+            }
+            if (left.val != right.val) {
+                return false;
+            }
+            queue.offer(left.left);
+            queue.offer(right.right);
+            queue.offer(left.right);
+            queue.offer(right.left);
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
         IsSymmetric_101 isSymmetric_101 = new IsSymmetric_101();
         TreeNode root = new TreeNode(1);
@@ -50,11 +85,13 @@ public class IsSymmetric_101 {
         root.right.left = new TreeNode(4);
         root.right.right = new TreeNode(3);
         System.out.println(isSymmetric_101.isSymmetric(root));
+        System.out.println(isSymmetric_101.isSymmetric2(root));
         root = new TreeNode(1);
         root.left = new TreeNode(2);
         root.right = new TreeNode(2);
         root.left.right = new TreeNode(3);
         root.right.right = new TreeNode(3);
         System.out.println(isSymmetric_101.isSymmetric(root));
+        System.out.println(isSymmetric_101.isSymmetric2(root));
     }
 }
